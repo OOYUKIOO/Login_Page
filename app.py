8346d3a59a40e1abb424446654e8703818f3967e
OOYUKIOO <yuxuan.chen.up@gmail.com>
2016-09-29 20:11:24 -0400

    Finished product of simple login page

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/auth", methods=['POST'])
+def loginCheck():
+    print request.form["usr"]
+    print request.form["pw"]
+    if request.form["usr"] == "Mario" and request.form["pw"] == "12345":
+        return "<center><b>Success!</b><center>"
+    else:
+        return "<center><b>Failure....</b><center>"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
